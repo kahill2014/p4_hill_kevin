4ac91d44a4e98394a10439dbb760e66458400c6e
Kevin Hill <kahill2014@gmail.com>
2020-04-21 09:42:49 -0500

    Added final touches and a few small clean-up items

--- js/fade.js
+++
@@ -1,4 +0,0 @@
-/* After the document loads, wait 2 seconds then fade out the intro overlay */
-setTimeout(function(){
-	$("#intro").addClass("hide");
-}, 2000);
--- js/main.js
+++ js/main.js
@@ -10,7 +10,7 @@ if($("#home").length){
 } else if($("#tutorials").length){
 	$("#tutorials").backstretch("../images/tutorials_background.jpg");
 } else if($("#product").length){
-	$("#product").backstretch("../images/shop_background.jpg");
+	$("#product").backstretch("../../images/shop_background.jpg");
 }
 
 /* Initialize Instagram feed plugin */
@@ -30,25 +30,25 @@ if($('#home').length){
 /* In the future, this would set the stage for admin functionality of adding/removing stock through the website */
 if ($("#shop-section").length){
 	var flies = [
-	{item:"Purple Feather Fly",product:"MC001",price:7.99,stock:4,featured:true,img:"../images/shop/item-1.png",description:"A feather fly used to help you catch largemouth bass."},
-	{item:"Green Feather Fly",product:"MC002",price:6.99,stock:4,featured:true,img:"../images/shop/item-2.png",description:"A feather fly used to help you catch largemouth bass."},
-	{item:"Red Feather Fly",product:"MC003",price:4.99,stock:4,featured:false,img:"../images/shop/item-3.png",description:"A feather fly used to help you catch largemouth bass."},
-	{item:"Yellow Feather Fly",product:"MC004",price:9.99,stock:4,featured:false,img:"../images/shop/item-4.png",description:"A feather fly used to help you catch largemouth bass."},
-	{item:"Orange Feather Fly",product:"MC005",price:5.99,stock:4,featured:false,img:"../images/shop/item-5.png",description:"A feather fly used to help you catch largemouth bass."},
-	{item:"Brown Feather Fly",product:"MC006",price:7.99,stock:4,featured:false,img:"../images/shop/item-6.png",description:"A feather fly used to help you catch largemouth bass."},
-	{item:"Black Feather Fly",product:"MC007",price:7.99,stock:4,featured:false,img:"../images/shop/item-7.png",description:"A feather fly used to help you catch largemouth bass."},
-	{item:"White Feather Fly",product:"MC008",price:6.99,stock:4,featured:false,img:"../images/shop/item-8.png",description:"A feather fly used to help you catch largemouth bass."},
-	{item:"Grey Feather Fly",product:"MC009",price:4.99,stock:4,featured:false,img:"../images/shop/item-9.png",description:"A feather fly used to help you catch largemouth bass."},
-	{item:"Pink Feather Fly",product:"MC010",price:9.99,stock:4,featured:false,img:"../images/shop/item-10.png",description:"A feather fly used to help you catch largemouth bass."},
-	{item:"Blue Feather Fly",product:"MC011",price:8.99,stock:4,featured:false,img:"../images/shop/item-11.png",description:"A feather fly used to help you catch largemouth bass."},
-	{item:"Rainbow Feather Fly",product:"MC012",price:5.99,stock:4,featured:false,img:"../images/shop/item-12.png",description:"A feather fly used to help you catch largemouth bass."},
+	{item:"Seaweed Dweller Fly",product:"MC001",price:7.99,stock:4,featured:true,img:"../images/shop/item-1.png",description:"A feather fly used to help you catch largemouth bass."},
+	{item:"Green Double Hooked Fly",product:"MC002",price:6.99,stock:4,featured:true,img:"../images/shop/item-2.png",description:"A feather fly used to help you catch largemouth bass."},
+	{item:"Purple Feather Fly",product:"MC003",price:4.99,stock:4,featured:false,img:"../images/shop/item-3.png",description:"A feather fly used to help you catch largemouth bass."},
+	{item:"Golden Frayed Fly",product:"MC004",price:9.99,stock:4,featured:false,img:"../images/shop/item-4.png",description:"A feather fly used to help you catch largemouth bass."},
+	{item:"White Fly",product:"MC005",price:5.99,stock:4,featured:false,img:"../images/shop/item-5.png",description:"A feather fly used to help you catch largemouth bass."},
+	{item:"Seaweed Covered Fly",product:"MC006",price:7.99,stock:4,featured:false,img:"../images/shop/item-6.png",description:"A feather fly used to help you catch largemouth bass."},
+	{item:"Camo Frog Fly",product:"MC007",price:7.99,stock:4,featured:false,img:"../images/shop/item-7.png",description:"A feather fly used to help you catch largemouth bass."},
+	{item:"Red and Blue Fly",product:"MC008",price:6.99,stock:4,featured:false,img:"../images/shop/item-8.png",description:"A feather fly used to help you catch largemouth bass."},
+	{item:"Small Shrimp Fly",product:"MC009",price:4.99,stock:4,featured:false,img:"../images/shop/item-9.png",description:"A feather fly used to help you catch largemouth bass."},
+	{item:"Green Insect Fly",product:"MC010",price:9.99,stock:4,featured:false,img:"../images/shop/item-10.png",description:"A feather fly used to help you catch largemouth bass."},
+	{item:"Frayed Camo Fly",product:"MC011",price:8.99,stock:4,featured:false,img:"../images/shop/item-11.png",description:"A feather fly used to help you catch largemouth bass."},
+	{item:"Orange Head Fly",product:"MC012",price:5.99,stock:4,featured:false,img:"../images/shop/item-12.png",description:"A feather fly used to help you catch largemouth bass."},
 	];
 
 	for(var i = 0; i < flies.length; i++){
 		var fly = document.createElement("div");
 		fly.classList.add("shop-item");
 		fly.setAttribute("id", flies[i].product)
-		var flyQuery = "<a href='products/" + flies[i].product + ".html'><img src='" + flies[i].img + "' /><div class='shop-text'><span class='title'>" + flies[i].item + "</span><span class='price'>$" + flies[i].price + "</span></div></a>";
+		var flyQuery = "<a href='products/MC001.html'><img alt='" + flies[i].item + "' src='" + flies[i].img + "' /><div class='shop-text'><span class='title'>" + flies[i].item + "</span><span class='price'>$" + flies[i].price + "</span></div></a>";
 		fly.insertAdjacentHTML("beforeend", flyQuery);
 		if (flies[i].featured){
 			fly.classList.add("shop-feature");
